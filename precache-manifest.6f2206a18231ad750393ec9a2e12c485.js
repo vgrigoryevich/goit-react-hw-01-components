6b54f137073a0f70c2e980a51a4865ace7c6c23c
vgrigoryevich <v.grigoryevich@gmail.com>
2020-04-16 15:43:54 +0300

    Updates

---
+++ precache-manifest.6f2206a18231ad750393ec9a2e12c485.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b1579f79cf58df8420205a227d5b59",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "dd241a2e991ca9cbfdc8",
+    "url": "/goit-react-hw-01-components/static/css/main.b4f2b659.chunk.css"
+  },
+  {
+    "revision": "a77aacc0367f5ee34e02",
+    "url": "/goit-react-hw-01-components/static/js/2.c2fa37ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.c2fa37ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd241a2e991ca9cbfdc8",
+    "url": "/goit-react-hw-01-components/static/js/main.e652397d.chunk.js"
+  },
+  {
+    "revision": "f2ebcb926d6c2cbee00b",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.62340070.js"
+  }
+]);
